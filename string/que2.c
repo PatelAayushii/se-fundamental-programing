d5cc80584aa17fc47db849f04335b3c3612b1403
PatelAayushii <ap4689943@gmail.com>
2024-09-16 13:26:34 +0530

    Add files via upload

---
+++ string/que2.c
@@ -0,0 +1,16 @@
+//2 : Write a program in C to separate individual characters from a string.
+#include<stdio.h>
+int main()
+{
+	char str[100];
+	int i=0;
+	printf("Enter any string:");
+	gets(str);
+	while(str[i]!='\0')
+	{
+		printf("\n Individual characters from a string = %c",str[i]);
+		i++;
+	}
+	
+	return 0;
+}
